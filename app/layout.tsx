e4be62b46cef84b9d86e1e3562fbcf199105d300
zaven <2avvv1n@gmail.com>
2024-12-02 14:03:27 +0800

    fix: repair bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,26 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
+// import type { Metadata } from "next";
+// import localFont from "next/font/local";
 import { Web3Provider } from "../src/providers/web3";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-  display: "swap",
-});
+// const geistSans = localFont({
+//   src: "./fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900",
+//   display: "swap",
+// });
 
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-  display: "swap",
-});
+// const geistMono = localFont({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+//   display: "swap",
+// });
 
-export const metadata: Metadata = {
-  title: "Next.js 应用",
-  description: "使用 Next.js 创建的应用",
-};
+// export const metadata: Metadata = {
+//   title: "Next.js 应用",
+//   description: "使用 Next.js 创建的应用",
+// };
 
 export default function RootLayout({
   children,
@@ -28,7 +28,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="zh" className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html lang="zh">
       <body className="antialiased">
         <Web3Provider>{children}</Web3Provider>
       </body>
